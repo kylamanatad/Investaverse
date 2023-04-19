7cc395b47c81acd4a8ee468ca084712f16eab432
Kyla <kylaguidomanatad@gmail.com>
2023-04-19 20:38:04 +0800

    Initial commit

---
+++ app/src/main/java/com/example/investaverse/Login.java
@@ -0,0 +1,38 @@
+package com.example.investaverse;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+public class Login {
+    private EditText username;
+    private EditText password;
+    private Button login;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        username = findViewById(R.id.username);
+        password = findViewById(R.id.pass);
+        login = findViewById(R.id.login);
+
+        login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (username.getText().length() > 0 && password.getText().length() > 0) {
+                    String toastMessage = "Username: " + username.getText().toString() + ", Password: " + password.getText().toString();
+                    Toast.makeText(getApplicationContext(), toastMessage, Toast.LENGTH_SHORT).show();
+                } else {
+                    String toastMessage = "Username or Password are not populated";
+                    Toast.makeText(getApplicationContext(), toastMessage, Toast.LENGTH_SHORT).show();
+                }
+            }
+        });    }
+}
